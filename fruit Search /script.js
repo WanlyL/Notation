9d421a43d0b104925572f14bde6fdad84faacf17
Wanly <wanly.leger@yahoo.com>
2023-09-08 13:58:57 -0400

    FruitSearch

---
+++ fruit Search /script.js
@@ -0,0 +1,34 @@
+let availableFruit = [
+    'Apple',
+    'Banana',
+    'Pear',
+    'Tomato',
+    'Mango',
+    'Pineapple',
+    'Strawberry',
+];
+
+const resultsBox = document.querySelector(".result-box");
+
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function(){
+    let result = [];
+    let input = inputBox.value;
+
+    if(input.length){
+        result = availableFruit.filter((keyword)=>{
+          return keyword.toLowerCase().includes(input.toLowerCase)
+        });
+        console.log(result);
+    }
+    display(result);
+}
+
+function display(result){
+    const content = result.map((list)=>{
+        return "<li>" + list + "</li>";
+    });
+
+    resultsBox.innerHTML = "<ul>" + content + "</ul>";
+}
