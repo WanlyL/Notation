45a7fe67f428e6389201ccc1f773af774727f5fe
Wanly <wanly.leger@yahoo.com>
2023-09-09 21:45:46 -0400

    revision

--- fruit Search /script.js
+++ fruit Search /script.js
@@ -8,30 +8,31 @@ let availableFruit = [
     'Strawberry',
 ];
 
-const resultsBox = document.querySelector(".result-box");
+const resultsBox = document.querySelector ('.result-box');
 
-const inputBox = document.getElementById("input-box");
+const inputBox = document.getElementById ('input-box');
 
-inputBox.onkeyup = function(){
+inputBox.onkeyup = function fruitSelection () {
     let result = [];
     let input = inputBox.value;
 
-    if(input.length){
-        result = availableFruit.filter((keyword)=>{
-          return keyword.toLowerCase().includes(input.toLowerCase)
+    if(input.length) {
+        result = availableFruit.filter((keyword) => {
+          return keyword.toLowerCase().includes (input.toLowerCase)
         });
-        console.log(result);
+        console.log (result);
     }
     display(result);
 }
 
-function display(result){
-    const content = result.map((list)=>{
-        return "<li>" + list + "</li>";
+function display (result) {
+    const content = result.map ((list) => {
+        return '<li>' + list + '</li>' ;
     });
 
-    resultsBox.innerHTML = "<ul>" + content + "</ul>";
+    resultsBox.innerHTML = '<ul>' + content + '</ul>' ;
 }
+
 
 var Jasmine = require('jasmine');
 var jasmine = new Jasmine();
